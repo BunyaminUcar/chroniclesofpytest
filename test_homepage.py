fa190a5ba225334252a7b4e2021f6c2b8eb2b049
Benjamin <57809668+BunyaminUcar@users.noreply.github.com>
2023-03-14 14:04:07 +0300

    Update test_homepage.py

--- test_homepage.py
+++ test_homepage.py
@@ -1,6 +1,7 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
+from pages.anasayfa import Anasayfa
 from selenium_framework import Selenium
 import pytest
 
@@ -18,21 +19,14 @@ class TestPage:
     
     def test_menu(self):
         self.driver.get("https://www.fullprogramlarindir.net/")
-        self.driver.implicitly_wait(10)
-        elements = self.driver.find_elements(By.CSS_SELECTOR,"ul#menu-menu > li > a")  
-        menu=[]
-        for i in elements:
-            menu.append(i.text)
+        anasayfa=Anasayfa(self.driver)
+        menu = anasayfa.menu_itemlerini_getir()
         assert menu == ["ANASAYFA","FULL PROGRAM INDIR","GÜNCEL İŞLETIM SISTEMLER","CEP MOBIL","FULL OYUN İNDIR","ÇEŞIT","YARDIM & İSTEK !"]
     
     def test_urun_ismine_basinca_icerigi_gosteriliyor(self):
         self.driver.get("https://www.fullprogramlarindir.net/")
-        self.driver.implicitly_wait(10)
-        link = self.driver.find_element(By.XPATH,"//*[@id='icerik']/div[3]/div[1]/div/h1/a")
-        urun_adi = link.text.strip()
-        link.click()
-        self.driver.implicitly_wait(10)
-        new_page_element=self.driver.find_element(By.XPATH,"//*[@id='icerik-yazi']/div[2]/h1/a").text.strip()
+        anasayfa=Anasayfa(self.driver)
+        urun_adi,new_page_element = anasayfa.urun_ismine_basinca_icerigi_gosteriliyor()
         assert urun_adi == new_page_element
         
         
