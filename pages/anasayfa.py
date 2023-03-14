3187da14f46a57d9743212b2d6d13d6ddd2e0912
Benjamin <57809668+BunyaminUcar@users.noreply.github.com>
2023-03-14 14:03:58 +0300

    Update anasayfa.py

--- pages/anasayfa.py
+++ pages/anasayfa.py
@@ -2,7 +2,7 @@ import pytest
 from selenium.webdriver.common.by import By
 
 
-@pytest.mark.usefixtures("setup")
+
 class Anasayfa():
 
     def __init__(self, driver):
@@ -10,3 +10,19 @@ class Anasayfa():
         
     def anasayfadan_urun_sec(self,xpath):
         self.driver.find_element(By.XPATH,xpath).click()
+        
+    def menu_itemlerini_getir(self):
+        self.driver.implicitly_wait(10)
+        elements = self.driver.find_elements(By.CSS_SELECTOR,"ul#menu-menu > li > a")  
+        menu=[]
+        for i in elements:
+            menu.append(i.text)
+        return menu
+    def urun_ismine_basinca_icerigi_gosteriliyor(self):
+        self.driver.implicitly_wait(10)
+        link = self.driver.find_element(By.XPATH,"//*[@id='icerik']/div[3]/div[1]/div/h1/a")
+        urun_adi = link.text.strip()
+        link.click()
+        self.driver.implicitly_wait(10)
+        new_page_element=self.driver.find_element(By.XPATH,"//*[@id='icerik-yazi']/div[2]/h1/a").text.strip()
+        return urun_adi,new_page_element
